90f6eecfe571252cf6fac009323cd13002a0b37d
Nishanthh2 <717822f135@kce.ac.in>
2026-06-15 23:39:15 +0530

    use env api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://jersey-store-backend-production.up.railway.app/api'
+    baseURL: import.meta.env.VITE_API_URL || 'https://jersey-store-backend-production.up.railway.app/api'
 })
 
 api.interceptors.request.use((config) => {
